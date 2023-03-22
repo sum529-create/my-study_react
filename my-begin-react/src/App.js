bb38cc318ad2cd6b0ab8550766533a2f4cbefeff
smnoh <smnoh@ui-lab.co.kr>
2023-03-22 17:40:17 +0900

    # update keychain test push

--- my-begin-react/src/App.js
+++ my-begin-react/src/App.js
@@ -1,9 +1,16 @@
-import Button from "./Button";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0); // 렌더링 될때 마다 출력
+  const onClick = () => setCounter((e) => e + 1);
+  console.log("render");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div className="App">
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}> Click me </button>
     </div>
   );
 }
