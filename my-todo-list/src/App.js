fb74a27683ccc6f2a8bc8d1eeab8abfbb3ca4128
smnoh <smnoh@ui-lab.co.kr>
2023-04-13 09:40:55 +0900

    # finish my-todo-list project

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (e) => setToDo(e.target.value);
+  const onReset = () => setToDo("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (toDo === "") return;
+    setToDos((d) => [toDo, ...d]);
+    /*
+      const num = [1,2,3]
+      add 4
+      [num, 4] // [[1,2,3],4] (x)
+      [...num, 4] // [1,2,3,4] (o)
+    */
+    setToDo("");
+  };
+  const onlistReset = () => setToDos([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Do List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+        <button onClick={onReset}>Reset</button>
+      </form>
+      <button onClick={onlistReset}>list Reset</button>
+      <ul>
+        {toDos.map((e, i) => {
+          return <li key={i}>{e}</li>;
+        })}
+      </ul>
     </div>
   );
 }
