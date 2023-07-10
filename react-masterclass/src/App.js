77e702562eaa6d6e7bbe438b5fe33eb38c204c3e
smnoh <smnoh@ui-lab.co.kr>
2023-07-10 10:03:44 +0900

    #2.3 'As' and Attrs - 같은 style을 다른 태그로 적용시 'as'사용 / 공통된 속성 부여 시 'attrs' 사용

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -4,21 +4,38 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(p) => p.bgColor};
-  width: 100px;
-  height: 100px;
+// const Box = styled.div`
+//   background-color: ${(p) => p.bgColor};
+//   width: 100px;
+//   height: 100px;
+// `;
+
+// const Circle = styled(Box)`
+//   border-radius: ${(p) => p.bdRadius};
+// `;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-const Circle = styled(Box)`
-  border-radius: ${(p) => p.bdRadius};
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: aqua;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="tomato" />
-      <Circle bgColor="teal" bdRadius="50px" />
+      {/* <Box bgColor="tomato" />
+      <Circle bgColor="teal" bdRadius="50px" /> */}
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        notice
+      </Btn>
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
