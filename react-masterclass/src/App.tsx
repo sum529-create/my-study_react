cc43bae067a72c0f4351b4246cd9fcd6d57190a3
smnoh <smnoh@ui-lab.co.kr>
2023-07-25 13:55:22 +0900

    #3.3 Optional Props - 선택적 props값 추가

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -3,7 +3,7 @@ import Circle from "./Circle";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
+      <Circle bgColor="teal" borderColor="black" text="teal color" />
       <Circle bgColor="tomato" />
     </div>
   );
--- react-masterclass/src/Circle.tsx
+++ react-masterclass/src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface ContainerProps {
   bgColor: string;
+  borderColor: string;
 }
 
 const Container = styled.div<ContainerProps>`
@@ -9,11 +10,15 @@ const Container = styled.div<ContainerProps>`
   height: 200px;
   background-color: ${(p) => p.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(p) => p.borderColor};
+  text-align: center;
 `;
 
 interface CircleProps {
   // interface - object의 shagpe설명
   bgColor: string;
+  borderColor?: string; // props값이 선택적일 경우 '?'를 붙여준다.
+  text?: string;
 }
 
 interface PlayerShape {
@@ -26,8 +31,17 @@ const sayHello = (p: PlayerShape) =>
 
 sayHello({ name: "sumin", age: 10 });
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+  // borderColor props error 발생 -> Circle props로는 optional값이지만, Container props론 required값이기 때문
+  // 1) borderColor값이 없을 경우 white기본값으로 처리한다
+  // ... borderColor = {borderColor ?? "white"}
+  // 2) borderColor값이 없을 경우 bgColor와 동일시 한다
+  // ... borderColor = {borderColor ?? bgColor}
 }
 
 export default Circle;
