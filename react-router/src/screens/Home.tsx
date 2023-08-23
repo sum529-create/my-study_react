0613d9f804d5845c37d05fb9354d087bae875cc4
sum529-create <nosumin29@gmail.com>
2023-08-23 11:30:26 +0900

    #4.8 - useSearchParams - url에서 쿼리값을 가져오거나 쿼리값 수정

--- react-router/src/screens/Home.tsx
+++ react-router/src/screens/Home.tsx
@@ -1,7 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { users } from "../db";
 
 function Home() {
+  // const [readSearchParams, setSearchParams] = useSearchParams();
+  // console.log(readSearchParams.has("followers"));
+  // setTimeout(() => {
+  //   setSearchParams({
+  //     day: "0823",
+  //     week: "wed",
+  //   });
+  // }, 3000);
+  // console.log(readSearchParams.get("day"));
+
   return (
     <div>
       <h1>Users</h1>
