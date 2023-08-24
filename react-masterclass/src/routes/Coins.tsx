f8045938bd2a93a0e314cb7572578ff677d2a955
sum529-create <nosumin29@gmail.com>
2023-08-24 15:48:47 +0900

    #5.3 Home part Two - api 연동(axios / fetch) + 내부 서버제한조치로 정적 데이터 사용

--- react-masterclass/src/routes/Coins.tsx
+++ react-masterclass/src/routes/Coins.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 const Header = styled.header`
   height: 10vh;
@@ -33,48 +37,87 @@ const Title = styled.h1`
   font-size: 48px;
   color: ${(p) => p.theme.accentColor};
 `;
-function Coins() {
-  const Coins = [
-    {
-      id: "btc-bitcoin",
-      name: "Bitcoin",
-      symbol: "BTC",
-      rank: 1,
-      is_new: false,
-      is_active: true,
-      type: "coin",
-    },
-    {
-      id: "eth-ethereum",
-      name: "Ethereum",
-      symbol: "ETH",
-      rank: 2,
-      is_new: false,
-      is_active: true,
-      type: "coin",
-    },
-    {
-      id: "hex-hex",
-      name: "HEX",
-      symbol: "HEX",
-      rank: 3,
-      is_new: false,
-      is_active: true,
-      type: "token",
-    },
-  ];
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
 
+const testCoins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  // useEffect : 특정한 시기에만 코드를 실행시켜줌(ex. componenet가 시작할 경우? 아님 끝날경우?)
+  // ()() 바로 function을 실행 할 수 있음
+  // 만약 console을 바로 실행하고 싶을 경우
+  // useEffect(() => {(console.log('something!'))()},[])
+
+  const getCoins = async () => {
+    // fetch 방법
+    // const res2 = await fetch("https://api.coinpaprika.com/v1/coins");
+    // const json = await await res2.json();
+
+    // axios 사용 -> json 변환 번거로움 감소
+    const res = await axios("https://api.coinpaprika.com/v1/coins");
+    setCoins(res.data.slice(0, 100));
+    setLoading(false);
+  };
+  useEffect(() => {
+    setLoading(false); // server 내부 에러로 임시 처리
+    // getCoins(); // server 내부 에러로 임시로 testCoin값 사용
+  }, []);
   return (
     <Container>
       <Header>
         <Title>Coins</Title>
       </Header>
       <CoinList>
-        {Coins.map((e) => (
-          <Coin key={e.id}>
-            <Link to={`/${e.id}`}> {e.name} &rarr;</Link>
-          </Coin>
-        ))}
+        {loading ? (
+          <Loader />
+        ) : (
+          testCoins.map((e) => (
+            <Coin key={e.id}>
+              <Link to={`/${e.id}`}> {e.name} &rarr;</Link>
+            </Coin>
+          ))
+        )}
       </CoinList>
     </Container>
   );
