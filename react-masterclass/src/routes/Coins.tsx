a97c2944fc849044896d70ed154964f4405890d0
sum529-create <nosumin29@gmail.com>
2023-09-14 09:31:16 +0900

    # Test data delete

--- react-masterclass/src/routes/Coins.tsx
+++ react-masterclass/src/routes/Coins.tsx
@@ -63,36 +63,6 @@ interface ICoin {
   type: string;
 }
 
-const testCoins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
-
 function Coins() {
   // const [coins, setCoins] = useState<ICoin[]>([]);
   // const [loading, setLoading] = useState(true);
