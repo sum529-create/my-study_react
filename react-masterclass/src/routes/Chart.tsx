9fa879fb353d4854ee4b60dd21c4c491956bee31
sum529-create <nosumin29@gmail.com>
2023-09-14 15:23:59 +0900

    #5.13 Price Chart part Two - Chart data 차트 시각화(apexcharts사용)

--- react-masterclass/src/routes/Chart.tsx
+++ react-masterclass/src/routes/Chart.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { useOutletContext } from "react-router-dom";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
@@ -19,11 +20,57 @@ interface IHistory {
 
 function Chart() {
   const { coinId } = useOutletContext<ChartProps>();
-  const { isLoading, data } = useQuery<IHistory>(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistory[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
 
-  return <h1>Chart</h1>;
+  return (
+    <h1>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "sales",
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 500,
+              width: 1000,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "stepline",
+              width: 4,
+            },
+            yaxis: {
+              show: true,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: true },
+              categories: data?.map((date) => {
+                const time = new Date(date.time_close * 1000);
+                return time.toLocaleDateString();
+              }),
+            },
+          }}
+        />
+      )}
+    </h1>
+  );
 }
 
 export default Chart;
