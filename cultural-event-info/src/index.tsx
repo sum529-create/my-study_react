d0aedfe9081c2b68d10258d7b11ba95b80166feb
sum529-create <nosumin29@gmail.com>
2023-10-20 17:28:22 +0900

    #cultural evnet info - header & searchArea 추가 + favicon 변경

--- cultural-event-info/src/index.tsx
+++ cultural-event-info/src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
