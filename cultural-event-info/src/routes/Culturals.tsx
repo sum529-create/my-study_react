7cdfa445446813df26ce6b63a125131ea17a8d91
sum529-create <nosumin29@gmail.com>
2023-12-05 11:16:00 +0900

    #loading 아이콘 추가 & 모바일 스크롤 페이징 구현 (수정)

--- cultural-event-info/src/routes/Culturals.tsx
+++ cultural-event-info/src/routes/Culturals.tsx
@@ -83,6 +83,257 @@ const Total = styled.div`
     font-size: 18px;
   }
 `;
+const Loading = styled.div`
+  @keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @-moz-keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @-webkit-keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @-o-keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @-moz-keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @-webkit-keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @-o-keyframes rotate-loading {
+    0% {
+      transform: rotate(0deg);
+      -ms-transform: rotate(0deg);
+      -webkit-transform: rotate(0deg);
+      -o-transform: rotate(0deg);
+      -moz-transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+      -ms-transform: rotate(360deg);
+      -webkit-transform: rotate(360deg);
+      -o-transform: rotate(360deg);
+      -moz-transform: rotate(360deg);
+    }
+  }
+
+  @keyframes loading-text-opacity {
+    0% {
+      opacity: 0;
+    }
+    20% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  @-moz-keyframes loading-text-opacity {
+    0% {
+      opacity: 0;
+    }
+    20% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  @-webkit-keyframes loading-text-opacity {
+    0% {
+      opacity: 0;
+    }
+    20% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+
+  @-o-keyframes loading-text-opacity {
+    0% {
+      opacity: 0;
+    }
+    20% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+  .loading-container,
+  .loading {
+    height: 100px;
+    position: relative;
+    width: 100px;
+    border-radius: 100%;
+  }
+
+  .loading-container {
+    margin: 50% auto;
+  }
+
+  .loading {
+    border: 2px solid transparent;
+    border-color: transparent #000 transparent #000;
+    -moz-animation: rotate-loading 1.5s linear 0s infinite normal;
+    -moz-transform-origin: 50% 50%;
+    -o-animation: rotate-loading 1.5s linear 0s infinite normal;
+    -o-transform-origin: 50% 50%;
+    -webkit-animation: rotate-loading 1.5s linear 0s infinite normal;
+    -webkit-transform-origin: 50% 50%;
+    animation: rotate-loading 1.5s linear 0s infinite normal;
+    transform-origin: 50% 50%;
+  }
+
+  .loading-container:hover .loading {
+    border-color: transparent #e45635 transparent #e45635;
+  }
+  .loading-container:hover .loading,
+  .loading-container .loading {
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+  }
+
+  #loading-text {
+    -moz-animation: loading-text-opacity 2s linear 0s infinite normal;
+    -o-animation: loading-text-opacity 2s linear 0s infinite normal;
+    -webkit-animation: loading-text-opacity 2s linear 0s infinite normal;
+    animation: loading-text-opacity 2s linear 0s infinite normal;
+    color: #000;
+    font-family: "Helvetica Neue", "Helvetica", "arial";
+    font-size: 10px;
+    font-weight: bold;
+    margin-top: 45px;
+    opacity: 0;
+    position: absolute;
+    text-align: center;
+    text-transform: uppercase;
+    top: 0;
+    width: 100px;
+  }
+`;
 interface ICulturalResponse {
   list_total_count: number;
   row: IFestival[];
@@ -336,43 +587,47 @@ function Culturals() {
       setFetchData(data);
     }
   }, [data]);
-  const isFetchingMoreData = useRef(false); // 추가 데이터 요청 중 여부
+  const [isLoadingMoreData, setIsLoadingMoreData] = useState(false); // 추가 데이터 로딩 중 여부
+  const [hasMoreData, setHasMoreData] = useState(true); // 더 이상 데이터가 없는지 여부
 
   useEffect(() => {
     function handleScroll() {
+      if (isLoading) {
+        return;
+      }
       const { scrollY, innerHeight } = window;
       const { offsetHeight } = document.body;
 
-      if (
-        window.innerWidth <= 768 &&
-        scrollY + innerHeight >= offsetHeight &&
-        !isFetchingMoreData.current // 추가 데이터 요청 중이 아닐 때만 실행
-      ) {
-        isFetchingMoreData.current = true; // 추가 데이터 요청 중으로 설정
-        var culturalDate = " ";
-        if (selectedDate) {
-          culturalDate = formateDate(selectedDate);
-        }
-        // API 요청
-        fetchCulturalInfo(startIdx, endIdx, {
-          codeNm: selectCodeNm ? selectCodeNm : " ",
-          title: searchTitle.current.value ? searchTitle.current.value : " ",
-          date: selectedDate ? culturalDate : " ",
-        })
-          .then((newData) => {
-            if (newData) {
-              setFetchData((prevData) => ({
-                ...prevData,
-                row: [...(prevData?.row || []), ...newData.row],
-                list_total_count: newData.list_total_count,
-              }));
-              setStartIdx((prevStartIdx) => prevStartIdx + 9);
-              setEndIdx((prevEndIdx) => prevEndIdx + 9);
-            }
+      if (window.innerWidth <= 768 && scrollY + innerHeight >= offsetHeight) {
+        setIsLoadingMoreData(true); // 추가 데이터 로딩 중으로 설정
+        setTimeout(() => {
+          var culturalDate = " ";
+          if (selectedDate) {
+            culturalDate = formateDate(selectedDate);
+          }
+          // API 요청
+          fetchCulturalInfo(startIdx, endIdx, {
+            codeNm: selectCodeNm ? selectCodeNm : " ",
+            title: searchTitle.current.value ? searchTitle.current.value : " ",
+            date: selectedDate ? culturalDate : " ",
           })
-          .finally(() => {
-            isFetchingMoreData.current = false; // 추가 데이터 요청 종료
-          });
+            .then((newData) => {
+              if (newData && newData.row.length > 0) {
+                setFetchData((prevData) => ({
+                  ...prevData,
+                  row: [...(prevData?.row || []), ...newData.row],
+                  list_total_count: newData.list_total_count,
+                }));
+                setStartIdx((prevStartIdx) => prevStartIdx + 9);
+                setEndIdx((prevEndIdx) => prevEndIdx + 9);
+              } else {
+                setHasMoreData(false); // 더 이상 데이터가 없음을 설정
+              }
+            })
+            .finally(() => {
+              setIsLoadingMoreData(false); // 추가 데이터 로딩 종료
+            });
+        }, 500);
       }
     }
 
@@ -381,7 +636,15 @@ function Culturals() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [startIdx, endIdx, selectCodeNm, selectedDate]);
+  }, [
+    isLoading,
+    startIdx,
+    endIdx,
+    selectCodeNm,
+    selectedDate,
+    isLoadingMoreData,
+    hasMoreData,
+  ]);
   const onClickSearch = async () => {
     const searchTit = searchTitle.current.value;
     setStartIdx(1);
@@ -492,7 +755,12 @@ function Culturals() {
         </Total>
         {fetchData && fetchData.list_total_count > 0 ? (
           isLoading ? (
-            "Loading..."
+            <Loading>
+              <div className="loading-container">
+                <div className="loading"></div>
+                <div id="loading-text">loading</div>
+              </div>
+            </Loading>
           ) : (
             <>
               <CultureList>
