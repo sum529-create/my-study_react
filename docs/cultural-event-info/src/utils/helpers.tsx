4b3b623fb6928a395edeeac774c977f6f52acdfc
sum529-create <nosumin29@gmail.com>
2024-01-02 11:08:56 +0900

    #create branch compare-coin

--- docs/cultural-event-info/src/utils/helpers.tsx
+++
@@ -1,9 +0,0 @@
-export const formateDate = (date: Date) => {
-  const myDate = new Date(date);
-
-  const year = myDate.getFullYear();
-  const month = String(myDate.getMonth() + 1).padStart(2, "0");
-  const day = String(myDate.getDate()).padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-};
