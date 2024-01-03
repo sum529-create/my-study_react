a1f62e6274a77382fa2ff4803766db94d8f3ab0c
sum529-create <nosumin29@gmail.com>
2024-01-03 09:47:45 +0900

    #movie app gh page upload testing

--- docs/movie-app/src/App.js
+++ docs/movie-app/src/App.js
@@ -4,19 +4,11 @@ import styles from "./css/App.module.css";
 import Detail from "./routes/Detail";
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route
-          className={styles.body}
-          path={process.env.PUBLIC_URL + "/movie/:id"}
-          element={<Detail />}
-        />{" "}
+        <Route className={styles.body} path="/movie/:id" element={<Detail />} />{" "}
         {/* ':' movie 하단 route값으로 원하는 값 추가가능 */}
-        <Route
-          className={styles.body}
-          path={process.env.PUBLIC_URL + "/"}
-          element={<Home />}
-        />
+        <Route className={styles.body} path="/" element={<Home />} />
       </Routes>
     </Router>
   );
