bbb1b7d1aff0b345618088980a7dd2ed82d135ff
sum529-create <nosumin29@gmail.com>
2024-01-08 14:27:10 +0900

    #프록시 설정 문제로 인한 코드 수정

--- docs/cultural-event-info/server.js
+++ docs/cultural-event-info/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 const proxyOptions = {
-  target: "http://openapi.seoul.go.kr:8088/",
+  target: "http://openapi.seoul.go.kr:8088",
   changeOrigin: true,
   pathRewrite: {
     "^/api": "/", // 프록시로 사용할 API 경로 설정
@@ -14,7 +14,10 @@ const proxyOptions = {
 };
 
 // "/api"로 시작하는 경로에 대해 프록시 미들웨어 생성
-app.use("/api", createProxyMiddleware(proxyOptions));
+app.use(
+  "/api/4d48766c7573756d31303757415a7157",
+  createProxyMiddleware(proxyOptions)
+);
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
