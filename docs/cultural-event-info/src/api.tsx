216400594fcf5b8b2b483c5cd3b093fd86b61866
sum529-create <nosumin29@gmail.com>
2024-01-08 11:29:19 +0900

    # cultural-event-info add proxy

--- docs/cultural-event-info/src/api.tsx
+++ docs/cultural-event-info/src/api.tsx
@@ -1,31 +1,8 @@
-// import axios from "axios";
-
-// const BASE_URL = `http://openapi.seoul.go.kr:8088/`;
-// const BASE_KEY = `4d48766c7573756d31303757415a7157`;
-
-// export const fetchCulturalInfo = async (
-//   startIdx: number,
-//   endIdx: number,
-//   options: {
-//     codeNm?: string;
-//     title?: string;
-//     date?: string;
-//   } = {}
-// ) => {
-//   const { codeNm, title, date } = options;
-//   let url = `${
-//     BASE_URL + BASE_KEY
-//   }/json/culturalEventInfo/${startIdx}/${endIdx}/`;
-
-//   if (codeNm) url += `${codeNm}/`;
-//   if (title) url += `${title}/`;
-//   if (date) url += `${date}/`;
-
-//   return await axios(url).then((e) => e.data.culturalEventInfo);
-// };
 import axios from "axios";
 
-const BASE_URL = `http://localhost:3002`;
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const BASE_URL = `http://openapi.seoul.go.kr:8088/`;
+const BASE_KEY = `4d48766c7573756d31303757415a7157`;
 
 export const fetchCulturalInfo = async (
   startIdx: number,
@@ -37,11 +14,41 @@ export const fetchCulturalInfo = async (
   } = {}
 ) => {
   const { codeNm, title, date } = options;
-  let url = `${BASE_URL}/api/culturalEventInfo/${startIdx}/${endIdx}/`;
+  let url = `${
+    BASE_URL + BASE_KEY
+  }/json/culturalEventInfo/${startIdx}/${endIdx}/`;
 
   if (codeNm) url += `${codeNm}/`;
   if (title) url += `${title}/`;
   if (date) url += `${date}/`;
 
-  return await axios.get(url).then((response) => response.data);
+  try {
+    const response = await axios.get(proxyUrl + url);
+    return response.data.culturalEventInfo;
+  } catch (error: any) {
+    console.error("Error fetching data:", error);
+    throw new Error(error.message);
+  }
 };
+// import axios from "axios";
+
+// const BASE_URL = `http://localhost:3002`;
+
+// export const fetchCulturalInfo = async (
+//   startIdx: number,
+//   endIdx: number,
+//   options: {
+//     codeNm?: string;
+//     title?: string;
+//     date?: string;
+//   } = {}
+// ) => {
+//   const { codeNm, title, date } = options;
+//   let url = `${BASE_URL}/api/culturalEventInfo/${startIdx}/${endIdx}/`;
+
+//   if (codeNm) url += `${codeNm}/`;
+//   if (title) url += `${title}/`;
+//   if (date) url += `${date}/`;
+
+//   return await axios.get(url).then((response) => response.data);
+// };
