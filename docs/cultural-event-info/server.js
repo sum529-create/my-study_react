9e6f00c4b6aab5683e8760b34d3ca98532750cd5
sum529-create <nosumin29@gmail.com>
2024-01-12 09:49:11 +0900

    #cultural-event-info change cors origin url

--- docs/cultural-event-info/server.js
+++ docs/cultural-event-info/server.js
@@ -5,7 +5,10 @@ const app = express();
 const cors = require("cors");
 // 서버 응답 헤더에 CORS 설정 추가
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://sum529-create.github.io");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://sum529-create.github.io/my-study_react/cultural-event-info"
+  );
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -16,7 +19,12 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(cors({ origin: "https://sum529-create.github.io" }));
+app.use(
+  cors({
+    origin:
+      "https://sum529-create.github.io/my-study_react/cultural-event-info",
+  })
+);
 
 app.use(express.json());
 
