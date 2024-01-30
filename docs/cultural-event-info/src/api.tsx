53db670bfb6a55a6b4dc295d1d5fd02e12158d1b
sum529-create <nosumin29@gmail.com>
2024-01-30 16:00:14 +0900

    #cultural-event-info add axios header

--- docs/cultural-event-info/src/api.tsx
+++ docs/cultural-event-info/src/api.tsx
@@ -21,7 +21,14 @@ export const fetchCulturalInfo = async (
   if (date) url += `${date}/`;
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      // 여기에 헤더를 추가합니다.
+      headers: {
+        withCredentials: true,
+        "Access-Control-Allow-Credentials": true,
+        "ngrok-skip-browser-warning": true,
+      },
+    });
     return response.data.culturalEventInfo;
   } catch (error: any) {
     console.error("Error fetching data:", error);
