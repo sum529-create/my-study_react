df372d5ca445b0f83914b9555275042689bea02e
sum529-create <nosumin29@gmail.com>
2024-05-22 17:42:03 +0900

    #cultural-event-info - 별도의 레포지토리로 이동으로 인한 삭제

--- docs/cultural-event-info/src/axiosInstance.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const axiosInstance = axios.create({
-  baseURL: 'https://my-study-react.glitch.me/', // 기본 URL 설정
-  timeout: 10000, // 요청 시간 초과 설정 (10초)
-});
-
-module.exports = axiosInstance;
